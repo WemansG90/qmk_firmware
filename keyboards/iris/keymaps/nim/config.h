140f10b175943f56ca24478e6067308939f81720
Nim <WemansG90@users.noreply.github.com>
2018-07-22 23:28:08 +0200

    changed SCL clock back to #define SCL_CLOCK  100000L

--- keyboards/iris/keymaps/nim/config.h
+++ keyboards/iris/keymaps/nim/config.h
@@ -15,10 +15,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#ifndef CONFIG_USER_H
-#define CONFIG_USER_H
-
-#include "config_common.h"
+#pragma once
 
 /* Use I2C or Serial, not both */
 
@@ -52,5 +49,3 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define QMK_ESC_INPUT D5 // usually ROW
 #define QMK_LED B0
 #define QMK_SPEAKER C6
-
-#endif
