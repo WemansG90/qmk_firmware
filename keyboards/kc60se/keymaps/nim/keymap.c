ac4ef90facb28c5333e3530e59ac3fc64976707f
Nim <WemansG90@users.noreply.github.com>
2018-07-29 17:23:56 +0200

    Edited KC60SE keymap

--- keyboards/kc60se/keymaps/nim/keymap.c
+++ keyboards/kc60se/keymaps/nim/keymap.c
@@ -74,6 +74,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,   KC_G,   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,           KC_ENT, \
       KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,   KC_B,   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
 <<<<<<< HEAD
+<<<<<<< HEAD
       MO(_FN2),KC_LALT, MO(_FN1),                 KC_SPC,                   KC_RALT, KC_LGUI, KC_APP,  MO(_FN3) \
       ),
   [_FN1] = LAYOUT_60_ansi( /* Layer 1 */
@@ -90,10 +91,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       xxxxxx,  KC_VOLD, KC_VOLU, KC_MUTE, xxxxxx, xxxxxx, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_LEFT, xxxxxx, xxxxxx, \
       xxxxxx,  KC_MPRV, KC_MPLY, KC_MNXT, xxxxxx, xxxxxx, KC_END, KC_PSLS, KC_PAST,  KC_PMNS, KC_PPLS, xxxxxx, \
 >>>>>>> Re-added custom kc60se map after git reset
+=======
+      MO(_FN2),KC_LALT, MO(_FN1),                 KC_SPC,                   KC_RALT, KC_LGUI, KC_APP,  MO(_FN3) \
+      ),
+  [_FN1] = LAYOUT_60_ansi( /* Layer 1 */
+      KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,  KC_F5,  KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_INS, \
+      KC_CAPS, BL_ON,   BL_STEP, xxxxxx,  xxxxxx, xxxxxx, xxxxxx,  KC_PGUP, KC_HOME, KC_PGDN, KC_PSCR, KC_SLCK, KC_PAUS, KC_DEL, \
+      xxxxxx,  KC_VOLD, KC_VOLU, KC_MUTE, xxxxxx, xxxxxx, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_LEFT,          xxxxxx,  xxxxxx, \
+      xxxxxx,           KC_MPRV, KC_MPLY, KC_MNXT, xxxxxx, xxxxxx, KC_END,  KC_PSLS, KC_PAST, KC_PMNS, KC_PPLS, xxxxxx, \
+>>>>>>> Edited KC60SE keymap
       xxxxxx,  xxxxxx,  xxxxxx,                   xxxxxx,                   xxxxxx,  KC_MSTP, xxxxxx,  xxxxxx \
       ),
   [_FN2] = LAYOUT_60_ansi( /* Layer 2 */
       xxxxxx,  xxxxxx,  xxxxxx,  xxxxxx,  xxxxxx, xxxxxx, xxxxxx,  xxxxxx,  xxxxxx,  xxxxxx,  xxxxxx,  xxxxxx,  xxxxxx, xxxxxx, \
+<<<<<<< HEAD
 <<<<<<< HEAD
       xxxxxx,  KC_P7,   KC_P8,   KC_P9,   xxxxxx, xxxxxx, xxxxxx,  xxxxxx,  xxxxxx,  xxxxxx,  xxxxxx,  xxxxxx,  xxxxxx, xxxxxx, \
       xxxxxx,  KC_P4,   KC_P5,   KC_P6,   xxxxxx, xxxxxx, xxxxxx,  xxxxxx,  xxxxxx,  xxxxxx,  xxxxxx,  xxxxxx,  xxxxxx, \
@@ -103,6 +114,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       xxxxxx,  KC_LEFT, KC_DOWN, KC_RGHT, xxxxxx, xxxxxx, xxxxxx,  xxxxxx,  xxxxxx,  xxxxxx,  xxxxxx,  xxxxxx,  xxxxxx, \
       xxxxxx,           xxxxxx,  xxxxxx,  xxxxxx, xxxxxx, xxxxxx,  xxxxxx,  xxxxxx,  xxxxxx,  xxxxxx,  xxxxxx,  xxxxxx, \
 >>>>>>> Re-added custom kc60se map after git reset
+=======
+      xxxxxx,  KC_P7,   KC_P8,   KC_P9,   xxxxxx, xxxxxx, xxxxxx,  xxxxxx,  xxxxxx,  xxxxxx,  xxxxxx,  xxxxxx,  xxxxxx, xxxxxx, \
+      xxxxxx,  KC_P4,   KC_P5,   KC_P6,   xxxxxx, xxxxxx, xxxxxx,  xxxxxx,  xxxxxx,  xxxxxx,  xxxxxx,  xxxxxx,  xxxxxx, \
+      xxxxxx,           KC_P1,   KC_P2,   KC_P3,  KC_P0,  xxxxxx,  xxxxxx,  xxxxxx,  xxxxxx,  xxxxxx,  xxxxxx,  xxxxxx, \
+>>>>>>> Edited KC60SE keymap
       xxxxxx,  xxxxxx,  xxxxxx,                   xxxxxx,                   xxxxxx,  xxxxxx,  xxxxxx,  xxxxxx \
       ),
   [_FN3] = LAYOUT_60_ansi( /* Layer 3 */
