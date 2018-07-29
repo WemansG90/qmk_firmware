2e59def0bc95f7898fad0859e494a4935ded3d33
Nim <WemansG90@users.noreply.github.com>
2018-07-29 17:23:56 +0200

    Edited KC60SE keymap

--- keyboards/kc60se/keymaps/nim/keymap.c
+++ keyboards/kc60se/keymaps/nim/keymap.c
@@ -57,20 +57,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC, \
       KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,   KC_G,   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,           KC_ENT, \
       KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,   KC_B,   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
-      MO(_FN2),KC_LALT, MO(_FN1),                 KC_SPC,                   KC_RALT,  KC_RALT, KC_LGUI, KC_APP \
+      MO(_FN2),KC_LALT, MO(_FN1),                 KC_SPC,                   KC_RALT, KC_LGUI, KC_APP,  MO(_FN3) \
       ),
   [_FN1] = LAYOUT_60_ansi( /* Layer 1 */
-      KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,  KC_F5,  KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, KC_INS, \
-      KC_CAPS, BL_ON,   BL_STEP, xxxxxx,  xxxxxx, xxxxxx, xxxxxx,  KC_PGUP,  KC_HOME, KC_PGDN, KC_PSCR, KC_SLCK, KC_PAUS, KC_DEL, \
-      xxxxxx,  KC_VOLD, KC_VOLU, KC_MUTE, xxxxxx, xxxxxx, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_LEFT, xxxxxx, xxxxxx, \
-      xxxxxx,  KC_MPRV, KC_MPLY, KC_MNXT, xxxxxx, xxxxxx, KC_END, KC_PSLS, KC_PAST,  KC_PMNS, KC_PPLS, xxxxxx, \
+      KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,  KC_F5,  KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_INS, \
+      KC_CAPS, BL_ON,   BL_STEP, xxxxxx,  xxxxxx, xxxxxx, xxxxxx,  KC_PGUP, KC_HOME, KC_PGDN, KC_PSCR, KC_SLCK, KC_PAUS, KC_DEL, \
+      xxxxxx,  KC_VOLD, KC_VOLU, KC_MUTE, xxxxxx, xxxxxx, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_LEFT,          xxxxxx,  xxxxxx, \
+      xxxxxx,           KC_MPRV, KC_MPLY, KC_MNXT, xxxxxx, xxxxxx, KC_END,  KC_PSLS, KC_PAST, KC_PMNS, KC_PPLS, xxxxxx, \
       xxxxxx,  xxxxxx,  xxxxxx,                   xxxxxx,                   xxxxxx,  KC_MSTP, xxxxxx,  xxxxxx \
       ),
   [_FN2] = LAYOUT_60_ansi( /* Layer 2 */
       xxxxxx,  xxxxxx,  xxxxxx,  xxxxxx,  xxxxxx, xxxxxx, xxxxxx,  xxxxxx,  xxxxxx,  xxxxxx,  xxxxxx,  xxxxxx,  xxxxxx, xxxxxx, \
-      xxxxxx,  xxxxxx,  KC_UP,   xxxxxx,  xxxxxx, xxxxxx, xxxxxx,  xxxxxx,  xxxxxx,  xxxxxx,  xxxxxx,  xxxxxx,  xxxxxx, xxxxxx, \
-      xxxxxx,  KC_LEFT, KC_DOWN, KC_RGHT, xxxxxx, xxxxxx, xxxxxx,  xxxxxx,  xxxxxx,  xxxxxx,  xxxxxx,  xxxxxx,  xxxxxx, \
-      xxxxxx,           xxxxxx,  xxxxxx,  xxxxxx, xxxxxx, xxxxxx,  xxxxxx,  xxxxxx,  xxxxxx,  xxxxxx,  xxxxxx,  xxxxxx, \
+      xxxxxx,  KC_P7,   KC_P8,   KC_P9,   xxxxxx, xxxxxx, xxxxxx,  xxxxxx,  xxxxxx,  xxxxxx,  xxxxxx,  xxxxxx,  xxxxxx, xxxxxx, \
+      xxxxxx,  KC_P4,   KC_P5,   KC_P6,   xxxxxx, xxxxxx, xxxxxx,  xxxxxx,  xxxxxx,  xxxxxx,  xxxxxx,  xxxxxx,  xxxxxx, \
+      xxxxxx,           KC_P1,   KC_P2,   KC_P3,  KC_P0,  xxxxxx,  xxxxxx,  xxxxxx,  xxxxxx,  xxxxxx,  xxxxxx,  xxxxxx, \
       xxxxxx,  xxxxxx,  xxxxxx,                   xxxxxx,                   xxxxxx,  xxxxxx,  xxxxxx,  xxxxxx \
       ),
   [_FN3] = LAYOUT_60_ansi( /* Layer 3 */
