4733989dcdfef46be30cf53de65eed495b1359eb
Nim <WemansG90@users.noreply.github.com>
2018-09-23 01:02:29 +0200

    quefrency initial map

---
+++ keyboards/quefrency/keymaps/nim/rules.mk
@@ -0,0 +1,12 @@
+BOOTMAGIC_ENABLE  = no      # Virtual DIP switch configuration(+1000)
+MOUSEKEY_ENABLE   = no       # Mouse keys(+4700)
+EXTRAKEY_ENABLE   = yes       # Audio control and System control(+450)
+CONSOLE_ENABLE    = no         # Console for debug(+400)
+COMMAND_ENABLE    = no        # Commands for debug and configuration
+AUDIO_ENABLE      = no
+NKRO_ENABLE	      = yes
+BOOTLOADER 	      = qmk-dfu
+
+ifndef QUANTUM_DIR
+	include ../../../../Makefile
+endif
