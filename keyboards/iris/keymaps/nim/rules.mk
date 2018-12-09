0d44ae2ad44afc9f99626aa78e6a9ee42ac69a97
Nim <WemansG90@users.noreply.github.com>
2018-12-09 23:01:22 +0100

    Prepwork for Planck map

--- keyboards/iris/keymaps/nim/rules.mk
+++ keyboards/iris/keymaps/nim/rules.mk
@@ -1,8 +1,8 @@
 BOOTMAGIC_ENABLE  = no      # Virtual DIP switch configuration(+1000)
-MOUSEKEY_ENABLE   = no       # Mouse keys(+4700)
-EXTRAKEY_ENABLE   = yes       # Audio control and System control(+450)
-CONSOLE_ENABLE    = no         # Console for debug(+400)
-COMMAND_ENABLE    = no        # Commands for debug and configuration
+MOUSEKEY_ENABLE   = no      # Mouse keys(+4700)
+EXTRAKEY_ENABLE   = yes     # Audio control and System control(+450)
+CONSOLE_ENABLE    = no      # Console for debug(+400)
+COMMAND_ENABLE    = no      # Commands for debug and configuration
 AUDIO_ENABLE      = no
 NKRO_ENABLE	      = yes
 RGBLIGHT_ENABLE   = yes
