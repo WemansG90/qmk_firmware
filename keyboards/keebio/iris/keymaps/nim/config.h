745d63896b46c2d6462086cc882aa1e0a43d60e1
Nim <geoffrey@archvm.localdomain>
2019-05-03 17:18:14 +0200

    rgb change

--- keyboards/keebio/iris/keymaps/nim/config.h
+++ keyboards/keebio/iris/keymaps/nim/config.h
@@ -31,6 +31,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #undef  RGBLED_NUM
 #define RGBLED_NUM 12
+#define RGBLED_SPLIT { 6, 6 }
 #define RGBLIGHT_HUE_STEP 10
 #define RGBLIGHT_SAT_STEP 10
 #define RGBLIGHT_VAL_STEP 10
